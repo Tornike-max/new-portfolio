9ca1f86a3a0716f67153b0d95ffc627484fe827c
Tornike-max <84152983+Tornike-max@users.noreply.github.com>
2024-04-07 02:00:30 +0400

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import Intro from "@/components/Intro";
+import React from "react";
+
+const Home = () => {
+  return (
+    <main className="">
+      <Intro />
+    </main>
+  );
+};
+
+export default Home;
