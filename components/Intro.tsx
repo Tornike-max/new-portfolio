d9f403d9a327f2ef671d6ccd4f62cf7d42fb7148
Tornike-max <84152983+Tornike-max@users.noreply.github.com>
2024-04-29 16:29:34 +0400

    i fixed errors

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -30,13 +30,13 @@ const Intro = () => {
             }}
           >
             <Image
-              src="https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?fit=crop&w=368&h=368&q=100"
+              src="/me.jpg"
               alt="Owner"
               width={192}
               height={192}
               quality="95"
               priority={true}
-              className="rounded-full h-24 w-24 object-cover border-[0.35rem] border-white shadow-xl"
+              className="rounded-full h-24 w-24 object-cover bg-cover border-[0.35rem] border-white shadow-xl"
             />
           </motion.div>
 
